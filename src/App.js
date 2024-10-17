05b886a1fd969e44c728c3c17b105464bd3ab18d
KhanAfsar12 <ka484564@gmail.com>
2024-10-17 20:23:43 +0530

    Apna Cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/Navbar';
 
 function App() {
+  const productList = [
+    {
+      price: 99999,
+      name: "IPhone 10S Max",
+      quantity: 0,
+    },
+    {
+      price: 99994,
+      name: "Samsung Max Pro",
+      quantity: 0,
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+
+    <ProductList productList={productList}/>
+    {/* <Footer/> */}
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Pyzon
+          </a>
+          
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
